1311f66daf8406a3c3679a6a0e2131a37d72de1c
argiope <argiope42@gmail.com>
2011-09-21 03:11:59 -0700

    Lots of changes

--- diigo.tpl.php
+++ diigo.tpl.php
@@ -1,5 +1,58 @@
+<?php
+/**
+ * @file
+ * the template file for the diigo bookmarks.
+ *
+ * @param $bookmarks
+ * is an array of bookmark objects
+ *
+ * @param $bookmark
+ * a bookmark object it should contain the following properties:
+ *
+ * @param $bookmark->url
+ * the url of the bookmark
+ *
+ * @param $bookmark->title
+ *
+ * @param $bookmark->tags
+ *
+ * @param $bookmark->desc
+ *
+ * @param $bookmark->created_at
+ *
+ * @param $bookmark->updated_at
+ *
+ */
+?>
 <ul>
-  <li>
-  </li>
+  <?php foreach($bookmarks as $bookmark):?>
+    <li>
+      <?php //dpr($bookmark); ?>
+      <div>
+        <?php print $bookmark->favicon; ?>
+        <?php print $bookmark->link; ?>
+      </div>
+      <?php if($bookmark->description): ?>
+        <div>
+          <?php print $bookmark->description; ?>
+        </div>
+      <?php endif; ?>
+      <?php if($bookmark->tags): ?>
+        <div>
+          <?php foreach($bookmark->tags as $tag): ?>
+            <span><?php print $tag; ?></span>
+          <?php endforeach; ?>
+        </div>
+      <?php endif; ?>
+      <div>
+        <?php print $bookmark->created_at; ?>
+      </div>
+      <?php if($bookmark->updated_at): ?>
+        <div>
+          <?php print $bookmark->updated_at; ?>
+        </div>
+      <?php endif; ?>
+    </li>
+  <?php endforeach; ?>
 </ul>
 
